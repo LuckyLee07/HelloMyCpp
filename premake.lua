d2e3b11918c58824311870e6f41804314370da4a
LuckyLee <071427li@163.com>
2024-03-13 00:02:08 +0800

    [dev]新增tolua++库

--- premake.lua
+++ premake.lua
@@ -4,35 +4,24 @@
 work_dir = "build"
 workspace "HelloMyCpp" -- 解决方案
 	location( work_dir )
-    startproject( "Lesson0_HelloLua" )
+    startproject( "HelloMyCpp" ) -- 设置启动项
+
+    platforms { "Win32", "Win64" }
+
     configurations { "Debug", "Release" }
 
     filter "platforms:Win32"
         system "Windows"
         architecture "x32"
-    filter{}
+
     filter "platforms:Win64"
         system "Windows"
         architecture "x86_64"
-    filter{}
-    filter "platforms:Mac"
-        system "macosx"
-        architecture "arm64"
-    filter{}
 
 
 proj_dir = work_dir .. "/%{prj.name}"
 bin_dir = proj_dir .. "/bin/%{cfg.buildcfg}"
 obj_dir = proj_dir .. "/obj/%{cfg.buildcfg}"
-
-
-function do_link( linkers )
-    for index, linker in pairs(linkers) do
-        links ( linker.lib_name )
-        libdirs  ( linker.lib_dirs )
-        sysincludedirs( linker.lib_include )
-    end
-end
 
 
 function CreateProject(projname, projtype, srcfiles, linkers)
@@ -46,49 +35,68 @@ function CreateProject(projname, projtype, srcfiles, linkers)
 
         files( srcfiles )
         
-        filter "system:macosx"
-            if projtype == "StaticLib" then 
-                targetextension ".a"
-            end
-            postbuildcommands { "cp %{cfg.buildtarget.relpath} ../../bin/" }
-        filter{}
-        filter "system:windows"
-            postbuildcommands { "{COPY} %{cfg.buildtarget.relpath} \"../../bin/\"" }
-        filter{}
+        disablewarnings { "4996" }
 
-        if linkers then do_link(linkers) end
+        if linkers ~= nil then
+            for index, linker in pairs(linkers) do
+                links ( linker.lib_name )
+                libdirs  ( linker.lib_dst )
+                includedirs( linker.lib_include )
+            end
+        end
+
+        postbuildcommands {
+            --("{COPY} %{cfg.buildtarget.relpath} \"../../bin/\"")
+        }
 end
 
-
 -- 现有第三方库
-local Linkers = {
-    {
-        lib_dirs = "./bin",
-        lib_name = "LuaLib", 
-        lib_include = "client/external/lua/lua",
-    },
-    {
-        lib_dirs = "./bin",
-        lib_name = "Frameworks", 
-        lib_include = "client/Frameworks",
-    },
+local Linkers = { }
+Linkers['lib_lua'] = 
+{
+    lib_dst = "./bin",
+    lib_name = "liblua.lib", 
+    lib_include = "client/external/lua/lua",
 }
+Linkers['lib_tolua'] = 
+{
+    lib_dst = "./bin",
+    lib_name = "libtolua.lib", 
+    lib_include = "client/external/lua/lua",
+}
+Linkers['lib_self'] = 
+{
+    lib_dst = "./bin",
+    lib_name = "frameworks.lib", 
+    lib_include = "client/Frameworks",
+}
+--------------External--------------
+group "External" -- 先创建文件夹组 --
 
 -- 静态库/第三方库
-CreateProject("LuaLib", "StaticLib", {
+CreateProject("liblua", "StaticLib", {
     "client/external/lua/lua/**.h",
     "client/external/lua/lua/**.c",
 })
-CreateProject("Frameworks", "StaticLib", {
+CreateProject("libtolua", "StaticLib", {
+    "client/external/lua/tolua/**.h",
+    "client/external/lua/tolua/**.c",
+}, { Linkers.lib_lua })
+
+group "" -- 结束group文件夹组 --
+--------------External--------------
+
+CreateProject("frameworks", "StaticLib", {
     "client/%{prj.name}/**.h",
     "client/%{prj.name}/**.cpp"
-}, { Linkers[1] })
+}, { Linkers.lib_lua })
 
+CreateProject("tolua++", "ConsoleApp", {
+    "client/external/lua/tolua/**.h",
+    "client/external/lua/tolua/**.c",
+}, { Linkers.lib_lua })
 
--- 实际项目
 CreateProject("HelloMyCpp", "ConsoleApp", {
     "client/%{prj.name}/**.h",
-    "client/%{prj.name}/**.cpp"
-}, { Linkers[1], Linkers[2] })
-
-
+    "client/%{prj.name}/**.cpp",
+}, Linkers )
---
+++ vs2022.bat
@@ -0,0 +1,26 @@
+@echo off
+
+set SCRIPT_DIRECTORY=%~dp0
+set CURRENT_DIRECTORY=%cd%
+set ARGUMENTS=%*
+
+cd /d %SCRIPT_DIRECTORY%
+
+if exist build (
+	cd build
+	del /S/Q *.sln *.*
+	cd ..
+) else (
+	mkdir build
+)
+
+cd bin
+del /S/Q *.log *.pdb *.exe
+cd ..
+
+echo on
+tools\premake\premake5 --os=windows --file=premake.lua vs2022 %ARGUMENTS%
+
+cd /d %CURRENT_DIRECTORY%
+
+pause
